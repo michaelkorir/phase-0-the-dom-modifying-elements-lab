c78d2900496c7afd63dc1116432ff9255a82a8a1
michael-korir <michaelkiptoo2000@gmail.com>
2023-10-13 12:12:56 +0300

    DOM manipulation

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+//Remove element using DOM
+const element = document.getElementById("main");
+element.remove();
+//Create elements using DOM
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
